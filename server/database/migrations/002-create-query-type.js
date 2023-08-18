00086056ea78a638c3e29ffafce44712cb490d8d
Anthony Volk <anth.volk@gmail.com>
2023-08-18 01:25:19 -0600

    feat: Migrations for Query and Query_Type models

---
+++ server/database/migrations/002-create-query-type.js
@@ -0,0 +1,20 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Query_Type', {
+			query_type_id: {
+				allowNull: false,
+				unique: true,
+				primaryKey: true,
+				type: Sequelize.UUID
+			},
+			query_type: {
+				type: Sequelize.STRING
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Query_Type');
+	}
+};
---
+++ server/database/migrations/003-create-query.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('Query', {
+			query_id: {
+				allowNull: false,
+				unique: true,
+				primaryKey: true,
+				type: Sequelize.UUID
+			},
+			user_id: {
+				allowNull: false,
+				type: Sequelize.UUID,
+				references: {
+					model: {
+						tableName: 'Users'
+					},
+					key: 'user_id'
+				}
+			},
+			input_json: {
+				type: Sequelize.JSON
+			},
+			input_full: {
+				type: Sequelize.TEXT
+			},
+			query_type_id: {
+				type: Sequelize.INTEGER
+			},
+			tokens_used: {
+				type: Sequelize.BIGINT
+			},
+			output_full: {
+				type: Sequelize.TEXT
+			}
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('Queries');
+	}
+};
