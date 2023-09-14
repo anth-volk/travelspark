0375d4a3bc9bdfdc5c5a8342b564286d12b68b03
Anthony Volk <anth.volk@gmail.com>
2023-09-13 23:40:29 -0600

    feat: Server is live

---
+++ server/app.js
@@ -0,0 +1,36 @@
+// Configure environment variables
+require('dotenv').config();
+
+// Essential package imports
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Express setup
+const express = require('express');
+const app = express();
+const port = process.env.NODE_ENV === 'production'
+  ? process.env.PORT
+  : 5050;
+
+// Express configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+// TODO: Route imports
+
+// ORM import
+const { Sequelize } = require('sequelize');
+
+// TODO: ORM config (ideally in separate file of some sort)
+
+// Middleware
+// TODO: Update CORS config when necessary
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Error in server configuration: ' + err);
+  }
+  console.log('App listening on port ' + port);
+});
